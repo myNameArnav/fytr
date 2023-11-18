61dcb43ec56a5595ebdf2441129ff5966a344928
Arnav <arnjain3@gmail.com>
2023-11-18 20:39:07 +0530

    used setInterval instead of setTimeout
    and a few changes

--- scripts/content.js
+++ scripts/content.js
@@ -1,6 +1,6 @@
 
-setTimeout(function () {
-    console.log("5 seconds have passed. Now executing the code.");
+setInterval(function () {
+    console.log("3 seconds has passed. Now executing the code.");
     var timeRegex = /\b(\d{1,2}:\d{1,2})\b/;
     var elements = document.getElementsByTagName("ytd-rich-item-renderer");
     var elementList = Array.from(elements);
@@ -9,12 +9,11 @@ setTimeout(function () {
         var txt = element.innerText;
         var match = txt.match(timeRegex);
         if (match) {
-            // console.log(txt, match[0]);
             var [hours, minutes, seconds] = match[0].split(':');
-            console.log(txt, hours, minutes, seconds)
-            if (hours < 10) {
-                element.style = "display: none;"
+            console.log(txt, hours, minutes, seconds);
+            if (parseInt(hours) < 10) {
+                element.style.display = "none";
             }
         }
-    })
-}, 10000);
+    });
+}, 3000);
