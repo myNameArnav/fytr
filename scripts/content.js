d42d4858bd3a900fd4857d3098ea3785c5b520f9
Arnav <arnjain3@gmail.com>
2023-11-24 00:08:09 +0530

    overhaul
    tidied up getTimeFilterChoice, added a reset func, used mutation observer instead of timeout and interval func

--- scripts/content.js
+++ scripts/content.js
@@ -1,19 +1,13 @@
+
+
 let firstRun = true
 
+
 async function getTimeFilterChoice(choice) {
-    if (choice == "short") {
-        // returns min and max seconds
-        return [0, 300]
-    }
-    else if (choice == "medium") {
-        return [301, 1200]
-    }
-    else if (choice == "long") {
-        return [1201, 1000000]
-    }
-    else {
-        return [0, 0]
-    }
+    if (choice == "short") return [0, 300]
+    else if (choice == "medium") return [301, 1200]
+    else if (choice == "long") return [1201, 1000000]
+    else return [0, 0]
 }
 
 async function getYtElements() {
@@ -25,15 +19,24 @@ async function getYtElementsLength() {
     return Array.from(document.getElementsByTagName("ytd-rich-item-renderer")).length;
 }
 
-
-async function convertTimeToSeconds(time){
-    if (time.split(':').length == 2){
-        time = "00:" + time
+async function resetView(){
+    let elementList = await getYtElements()
+    for (element of elementList){
+        element.style.display = "";
     }
-    const [hours, minutes, seconds] = time.split(':').map(Number);
-    const timeInSeconds = (parseInt(hours) * 3600) + (parseInt(minutes) * 60) + parseInt(seconds)
+}
 
-    return timeInSeconds
+async function convertTimeToSeconds(time) {
+    let timeLen = time.split(':').length;
+    if (timeLen >= 2) {
+        if (timeLen == 2) {
+            time = "00:" + time
+        }
+        const [hours, minutes, seconds] = time.split(':').map(Number);
+        const timeInSeconds = (parseInt(hours) * 3600) + (parseInt(minutes) * 60) + parseInt(seconds)
+        return timeInSeconds
+    }
+    return false
 }
 
 async function ytFilter(elementList) {
@@ -44,12 +47,12 @@ async function ytFilter(elementList) {
         let txt = element.innerText;
         if (!txt.includes("#shorts")) {
             let match = txt.substring(0, txt.indexOf("\n"));
-            if (match) {
-                let choiceInSeconds = await convertTimeToSeconds(match);
-                [choiceMin, choiceMax] = await getTimeFilterChoice("short");
+            let choiceInSeconds = await convertTimeToSeconds(match);
+            if (choiceInSeconds) {
+                [choiceMin, choiceMax] = await getTimeFilterChoice("medium");
                 let works = choiceInSeconds >= choiceMin && choiceInSeconds <= choiceMax
-                console.log({"text": txt.trim().slice(0, 25), "time": choiceInSeconds, "min": choiceMin, "max": choiceMax, "works": works});
-                if (!(choiceInSeconds >= choiceMin && choiceInSeconds <= choiceMax)) {
+                console.log({ "text": txt.trim().slice(0, 25), "time": choiceInSeconds, "min": choiceMin, "max": choiceMax, "works": works });
+                if (!works) {
                     element.style.display = "none";
                 }
             }
@@ -57,18 +60,12 @@ async function ytFilter(elementList) {
     }
 }
 
-async function stateDiffChecker() {
-    let ytElements = await getYtElementsLength();
-    setTimeout(async function () {
-        let ytElementsNew = await getYtElementsLength();
-
-        if (ytElements !== ytElementsNew) {
-            console.log({"state": "Different State"});
-            await ytFilter(await getYtElements());
-        } else {
-            console.log({"state": "Same State"});
-        }
-    }, 1000)
+async function handleStateChange(mutations) {
+    if (document.getElementById("time-status")){
+        console.log("State changed");
+        await ytFilter(await getYtElements());
+    }
 }
 
-setInterval(stateDiffChecker, 5000);
+const observer = new MutationObserver(handleStateChange);
+observer.observe(document.body, { subtree: true, childList: true });
