ac6403864a3b72742de318ead996dc5bc08dfd5f
Arnav <arnjain3@gmail.com>
2023-11-23 22:04:01 +0530

    works... kindof

--- scripts/content.js
+++ scripts/content.js
@@ -1,19 +1,74 @@
+let firstRun = true
 
-setInterval(function () {
-    console.log("3 seconds has passed. Now executing the code.");
-    var timeRegex = /\b(\d{1,2}:\d{1,2})\b/;
-    var elements = document.getElementsByTagName("ytd-rich-item-renderer");
-    var elementList = Array.from(elements);
+async function getTimeFilterChoice(choice) {
+    if (choice == "short") {
+        // returns min and max seconds
+        return [0, 300]
+    }
+    else if (choice == "medium") {
+        return [301, 1200]
+    }
+    else if (choice == "long") {
+        return [1201, 1000000]
+    }
+    else {
+        return [0, 0]
+    }
+}
 
-    elementList.forEach(element => {
-        var txt = element.innerText;
-        var match = txt.match(timeRegex);
-        if (match) {
-            var [hours, minutes, seconds] = match[0].split(':');
-            console.log(txt, hours, minutes, seconds);
-            if (parseInt(hours) < 10) {
-                element.style.display = "none";
+async function getYtElements() {
+    let elements = document.getElementsByTagName("ytd-rich-item-renderer");
+    return Array.from(elements);
+}
+
+async function getYtElementsLength() {
+    return Array.from(document.getElementsByTagName("ytd-rich-item-renderer")).length;
+}
+
+
+async function convertTimeToSeconds(time){
+    if (time.split(':').length == 2){
+        time = "00:" + time
+    }
+    const [hours, minutes, seconds] = time.split(':').map(Number);
+    const timeInSeconds = (parseInt(hours) * 3600) + (parseInt(minutes) * 60) + parseInt(seconds)
+
+    return timeInSeconds
+}
+
+async function ytFilter(elementList) {
+    console.log("Running ytFilter");
+    let choiceMax = 0, choiceMin = 0;
+
+    for (element of elementList) {
+        let txt = element.innerText;
+        if (!txt.includes("#shorts")) {
+            let match = txt.substring(0, txt.indexOf("\n"));
+            if (match) {
+                let choiceInSeconds = await convertTimeToSeconds(match);
+                [choiceMin, choiceMax] = await getTimeFilterChoice("short");
+                let works = choiceInSeconds >= choiceMin && choiceInSeconds <= choiceMax
+                console.log({"text": txt.trim().slice(0, 25), "time": choiceInSeconds, "min": choiceMin, "max": choiceMax, "works": works});
+                if (!(choiceInSeconds >= choiceMin && choiceInSeconds <= choiceMax)) {
+                    element.style.display = "none";
+                }
             }
         }
-    });
-}, 3000);
+    }
+}
+
+async function stateDiffChecker() {
+    let ytElements = await getYtElementsLength();
+    setTimeout(async function () {
+        let ytElementsNew = await getYtElementsLength();
+
+        if (ytElements !== ytElementsNew) {
+            console.log({"state": "Different State"});
+            await ytFilter(await getYtElements());
+        } else {
+            console.log({"state": "Same State"});
+        }
+    }, 1000)
+}
+
+setInterval(stateDiffChecker, 5000);
